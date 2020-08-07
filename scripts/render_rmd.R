8aaae600b101e1269fc53d71b4e4b55203831f31
epongpipat <epongpipat@gmail.com>
2020-08-07 13:43:32 -0700

    rename output to index.html

--- scripts/render_rmd.R
+++ scripts/render_rmd.R
@@ -4,4 +4,4 @@ packages <- c("rmarkdown", "glue")
 xfun::pkg_attach2(packages, message = F)
 
 render(input = glue("scripts/covid19.Rmd"), 
-       output_file = glue("docs/covid19.html"))
+       output_file = glue("docs/index.html"))
